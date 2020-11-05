83599a0196d0f075d3fd6995ce7d7683546bf5df
Michele Cantelli <emmekappa@gmail.com>
2020-11-05 15:45:48 +0100

    Config Connector Service Account

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ terraform {
     google-beta      = ">= 3.46.0"
     kubernetes       = ">= 1.13.3"
     helm             = ">= 1.3.2"
-    tls              = ">= 3.0.0"
+    tls              = ">= 2.1"
     kubernetes-alpha = ">= 0.2.1"
   }
 }
