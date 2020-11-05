c5b9e18635fe3bb57c056131ba1034033cf9f422
Michele Cantelli <emmekappa@gmail.com>
2020-11-05 13:48:42 +0100

    Using full name as git repo

--- modules/github/deploy_key.tf
+++ modules/github/deploy_key.tf
@@ -1,10 +1,10 @@
-data "github_repository" "flux-repo" {
+data "github_repository" "flux" {
   full_name = "${var.github_org_name}/${var.github_repository_name}"
 }
 
 resource "github_repository_deploy_key" "flux" {
   title      = "Flux deploy key (${var.cluster_name})"
-  repository = data.github_repository.flux-repo.name
+  repository = data.github_repository.flux.full_name
   read_only  = false
   key        = var.public_key_openssh
 }
