ff21a2356feb7cbeb7e3f501ed597120c92669e8
Michele Cantelli <emmekappa@gmail.com>
2020-11-05 16:24:36 +0100

    k8s alpha

--- terraform.tf
+++ terraform.tf
@@ -3,8 +3,8 @@ terraform {
     google           = ">= 3.46.0"
     google-beta      = ">= 3.46.0"
     kubernetes       = ">= 1.13.3"
+    kubernetes-alpha = ">= 0.2.1"
     helm             = ">= 1.3.2"
     tls              = ">= 2.1"
-    kubernetes-alpha = ">= 0.2.1"
   }
 }
