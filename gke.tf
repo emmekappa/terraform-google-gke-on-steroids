22405e471b03a5b09ad091119c7b1346cbc88dbb
Michele Cantelli <emmekappa@gmail.com>
2020-11-06 11:10:02 +0100

    Adding oauth scope https://www.googleapis.com/auth/cloud-platform

--- gke.tf
+++ gke.tf
@@ -6,7 +6,8 @@ locals {
     "https://www.googleapis.com/auth/service.management.readonly",
     "https://www.googleapis.com/auth/servicecontrol",
     "https://www.googleapis.com/auth/trace.append",
-    "https://www.googleapis.com/auth/ndev.clouddns.readwrite"
+    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+    "https://www.googleapis.com/auth/cloud-platform"
   ]
 }
 
